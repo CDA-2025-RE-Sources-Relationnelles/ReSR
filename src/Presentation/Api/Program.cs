d22fe969c628c62990d6ee04079acd9a5bbc011c
Lastelle <lastelle.dupont@gmail.com>
2026-03-04 15:42:37 +0100

    Removed launchSettings from .gitignore for easier setup

--- src/Presentation/Api/Program.cs
+++ src/Presentation/Api/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.EntityFrameworkCore;
 using ReSR.Application.Core;
-using ReSR.Domain.Aggregates.Accounts;
-using ReSR.Domain.Aggregates.Categories;
-using ReSR.Domain.Aggregates.Resources;
-using ReSR.Domain.Ports;
 using ReSR.Infrastructure.Core;
 using ReSR.Presentation.Api.Core.Extensions;
 
