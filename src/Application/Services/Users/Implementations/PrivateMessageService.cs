e25e0f0b92b15fb988608fc00242885ccb5ef9b2
Justine Loriot <loriot.justine@gmail.com>
2026-03-06 10:44:27 +0100

    Control if users are friends to send private messages

--- src/Application/Services/Users/Implementations/PrivateMessageService.cs
+++ src/Application/Services/Users/Implementations/PrivateMessageService.cs
@@ -4,9 +4,6 @@ using ReSR.Domain.Aggregates.Accounts;
 using ReSR.Domain.Aggregates.Messages;
 using ReSR.Domain.Ports;
 using ReSR.Application.Services.Users.Definitions;
-using System.Data.Common;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading.Tasks.Dataflow;
 
 namespace ReSR.Application.Services.Users.Implementations;
 
@@ -16,20 +13,26 @@ internal class PrivateMessageService(
 ) : IPrivateMessageService
 {
     public Task<IResponse<PrivateMessage>> TrySendAsync(
-        Id senderId, 
-        Id receiverId, 
-        string content
+    Id senderId, 
+    Id receiverId, 
+    string content
     ){
-            return userRepo.TryGetAsync(senderId)
-                .OnSuccessAsync(sender => 
-                    userRepo.TryGetAsync(receiverId)
-                        .OnSuccessAsync(receiver =>
-                            PrivateMessage
-                                .TryCreate(sender, receiver, content)
-                                .OnSuccessAsync(messageRepo.TryAddAsync)
-                        )
-                );
-        }
+        return userRepo.TryGetAsync(senderId)
+            .OnSuccessAsync(sender => 
+                userRepo.TryGetAsync(receiverId)
+                    .OnSuccessAsync(receiver =>
+                    {
+                        if (!sender.Friends.Any(f => f.Id == receiver.Id))
+                            return Task.FromResult(
+                                Response.Failure<PrivateMessage>("Vous devez être amis pour vous envoyer des messages !")
+                            );
+
+                        return PrivateMessage
+                            .TryCreate(sender, receiver, content)
+                            .OnSuccessAsync(messageRepo.TryAddAsync);
+                    })
+            );
+    }
 
     public Task<IResponse<IEnumerable<PrivateMessage>>> TryGetMessagesBetweenAsync(
         Id senderId, 
